53687f60beb82918ec1dc5e45333dc781c3f1fbf
khaima09 <khaima8@gmail.com>
2021-03-02 11:15:00 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,9 +4,9 @@ node default {
 node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
-    ensure => file,
-    content => “Welcome to ${fqdn}”,
-    owner => 'root',
+    ensure  => file,
+    content => ${fqdn},
+    owner   => 'root',
   }
 }
 node /^web/ {
